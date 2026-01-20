6f146f32db393d36eace611d93fc5373f4e1736a
alchemiesofscent <coughlin@flu.cas.cz>
2026-01-20 22:15:42 +0100

    chore: bundle Tailwind locally and add favicon

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
